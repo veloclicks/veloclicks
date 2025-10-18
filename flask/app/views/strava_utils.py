81277c443d55265c3604a4de3405b7395e9989aa
greyneuron <pllowry@hotmail.com>
2025-10-18 10:20:33 +0100

    Created zappa lambda wrapper for AWS. Changed database column data type for activity dates

--- flask/app/views/strava_utils.py
+++ flask/app/views/strava_utils.py
@@ -94,7 +94,14 @@ def get_key_activity_attributes(user_id, activity):
     keys=['id', 'name', 'start_date_local', 'start_date', 'type', 'distance', 'elapsed_time', 'moving_time', 'average_speed', 'max_speed', 'average_watts', 'max_watts', 'weighted_average_watts', 'average_heartrate', 'max_heartrate', 'suffer_score', 'average_cadence', 'total_elevation_gain', 'elev_low', 'elev_high']
     
     minimal_dict = {key: activity[key] for key in keys if key in activity}
-    
+
+    # Convert date strings to DateTime objects
+    if 'start_date' in minimal_dict and minimal_dict['start_date']:
+        minimal_dict['start_date'] = datetime.fromisoformat(minimal_dict['start_date'].replace('Z', '+00:00'))
+
+    if 'start_date_local' in minimal_dict and minimal_dict['start_date_local']:
+        minimal_dict['start_date_local'] = datetime.fromisoformat(minimal_dict['start_date_local'])
+
     # also put the user id into the dictionary
     minimal_dict['user_id'] = user_id
 
